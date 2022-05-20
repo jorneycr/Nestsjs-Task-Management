5ca321ff19a2f9f3cac0f3158e1113d44459bac0
jorneylopez <jorney95@live.com>
2022-05-19 21:18:02 -0600

    poast method

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -10,4 +10,10 @@ export class TasksController {
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
   }
+
+  @Post()
+  createTask(@Body('title') title: string, @Body('description') description: string): Task {
+    return this.tasksService.createTask(title, description);
+  }
+
 }
