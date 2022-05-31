916458b08fca4b0292f422b3597a81fed359b660
jorneylopez <jorney95@live.com>
2022-05-30 22:30:40 -0600

    add user service, and controller

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -6,6 +7,10 @@ import { InjectRepository } from '@nestjs/typeorm';
 export class AuthService {
     constructor(
         @InjectRepository(UsersRepository)
-        private usersRepository:UsersRepository,
-    ){}
+        private usersRepository: UsersRepository,
+    ) { }
+
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.usersRepository.createUser(authCredentialsDto);
+    }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,7 +1,15 @@
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { EntityRepository, Repository } from "typeorm";
 import { User } from "./user.entity";
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User>{
-
+    async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        const { username, password } = authCredentialsDto;
+        const user = this.create({
+            username,
+            password,
+        });
+        await this.save(user);
+    }
 }
