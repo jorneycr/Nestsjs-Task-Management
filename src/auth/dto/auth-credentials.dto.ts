c5120c068f2041d58918669dccd02f4022b64c81
jorneylopez <jorney95@live.com>
2022-05-30 22:37:33 -0600

    validation auth

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,14 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
 export class AuthCredentialsDto {
+    @IsString()
+    @MinLength(3)
+    @MaxLength(20)
     username: string;
+
+    @IsString()
+    @MinLength(3)
+    @MaxLength(20)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)
     password: string;
 }
