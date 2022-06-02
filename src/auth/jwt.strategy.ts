bfbb548e586126f27f3a3eb7e59054f388977921
jorneylopez <jorney95@live.com>
2022-06-01 21:35:01 -0600

    jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { JwtPayload } from './jwt-payload.interface';
+import { UsersRepository } from './users.repository';
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        @InjectRepository(UsersRepository)
+        private usersRepository: UsersRepository,
+    ) {
+        super({
+            secretOrKey: 'topSecret51',
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        })
+    }
+
+    async validate(payload: JwtPayload): Promise<User> {
+        const { username } = payload;
+        const user: User = await this.usersRepository.findOne({ username });
+
+        if (!user) {
+            throw new UnauthorizedException();
+        }
+
+        return user;
+    }
+}
